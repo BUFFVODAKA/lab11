29ed3ca28ff5b4383c6a5ad0759d2b253a225196
VODAKA <poottipong_k@cmu.ac.th>
2023-01-03 17:02:26 +0700

    lab11_3 finished

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+#include<cstdlib>
+#include<fstream>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string num;
+    double n;
+    double total;
+    double totalfordev;
+    while(getline(source,num)){
+        total += stod(num);
+        totalfordev += pow(stod(num),2);
+        n++;
+    }
+    double Mean = total/n;
+    double deviation = sqrt((totalfordev/n)-(pow(Mean,2)));
+    cout << "Number of data = " << n << endl;
+    cout << setprecision(3) ;
+    cout << "Mean = " << Mean << endl;
+    cout << "Standard deviation = "<< deviation << endl;
+
+
+    source.close();
+    return 0;
+}
